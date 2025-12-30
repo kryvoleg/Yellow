bf994d0d1b539d241cf811d794f9245d014da84d
kryvoleg <kryvosejoleg@gmail.com>
2025-12-30 07:06:07 +0100

    L3

--- contracts/Li1.sol
+++ contracts/Li1.sol
@@ -1,10 +1,55 @@
-
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.4.0
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Li1 is ERC20 {
-    constructor() ERC20("Li1", "MTK") {}
+contract RandomNameToken is ERC20 {
+    string private _nameInternal;
+    string private _symbolInternal;
+
+    constructor(uint256 initialSupply) ERC20("Ret", "RT") {
+        uint256 rand = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.prevrandao,
+                    block.timestamp,
+                    msg.sender
+                )
+            )
+        );
+
+        bytes memory alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        uint256 nameLen = 5 + (rand % 6);
+        bytes memory nameBytes = new bytes(nameLen);
+
+        for (uint256 i = 0; i < nameLen; i++) {
+            uint256 idx = uint8(
+                uint256(keccak256(abi.encodePacked(rand, i))) % alphabet.length
+            );
+            nameBytes[i] = alphabet[idx];
+        }
+
+        bytes memory symbolBytes = new bytes(3);
+        for (uint256 j = 0; j < 3; j++) {
+            uint256 idx2 = uint8(
+                uint256(keccak256(abi.encodePacked(rand, j + 100))) %
+                    alphabet.length
+            );
+            symbolBytes[j] = alphabet[idx2];
+        }
+
+        _nameInternal = string(nameBytes);
+        _symbolInternal = string(symbolBytes);
+
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
+    }
+
+    function name() public view override returns (string memory) {
+        return _nameInternal;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbolInternal;
+    }
 }
