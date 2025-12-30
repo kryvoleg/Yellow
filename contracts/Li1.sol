baf5fa6185b7cd18aa56ddfa279f3ace1c1d2be9
kryvoleg <kryvosejoleg@gmail.com>
2025-12-30 07:04:50 +0100

    Al2

---
+++ contracts/Li1.sol
@@ -0,0 +1,10 @@
+
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Li1 is ERC20 {
+    constructor() ERC20("Li1", "MTK") {}
+}
